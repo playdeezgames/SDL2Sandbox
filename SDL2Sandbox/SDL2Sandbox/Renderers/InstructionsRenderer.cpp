1cdac060ba88d0810321c7c410098d0b3409d12e
ernestpazera <ernestpazera@gmail.com>
2021-01-16 05:21:23 -0600

    instructions are "done"

--- SDL2Sandbox/SDL2Sandbox/Renderers/InstructionsRenderer.cpp
+++ SDL2Sandbox/SDL2Sandbox/Renderers/InstructionsRenderer.cpp
@@ -10,5 +10,11 @@ InstructionsRenderer::InstructionsRenderer(SDL_Renderer* rend, const RomFontMana
 void InstructionsRenderer::Draw()
 {
 	int row = Constants::Board::ROWS / 2;
-	GetRomFont().DrawCenteredText(GetMainRenderer(), row + 0, "Instructions", Constants::Color::WHITE);
+	GetRomFont().DrawCenteredText(GetMainRenderer(), row - 3, "-=-Instructions-=-", Constants::Color::WHITE);
+
+	GetRomFont().DrawCenteredText(GetMainRenderer(), row - 1, "Keyboard Controls:", Constants::Color::CYAN);
+	GetRomFont().DrawCenteredText(GetMainRenderer(), row + 0, "Arrows, Space, Esc", Constants::Color::MAGENTA);
+
+	GetRomFont().DrawCenteredText(GetMainRenderer(), row + 2, "Game Controller:", Constants::Color::CYAN);
+	GetRomFont().DrawCenteredText(GetMainRenderer(), row + 3, "LStick, (A), (Back)", Constants::Color::MAGENTA);
 }
