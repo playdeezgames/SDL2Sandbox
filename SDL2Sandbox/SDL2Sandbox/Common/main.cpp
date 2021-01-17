7c379991b243d4ebd791a83ede8d55b7de38c7f3
ernestpazera <ernestpazera@gmail.com>
2021-01-17 04:24:26 -0600

    refactored main.cpp

--- SDL2Sandbox/SDL2Sandbox/Common/main.cpp
+++ SDL2Sandbox/SDL2Sandbox/Common/main.cpp
@@ -1,13 +1,25 @@
 #include "Application.h"
 #include <stdlib.h>
 #include <time.h>
-int main(int argc, char** argv)
+
+static std::vector<std::string> CommandLineToStringVector(int argc, char** argv)
 {
-	srand((unsigned int)time(nullptr));
 	std::vector<std::string> arguments(argc);
 	for (int index = 0; index < argc; ++index)
 	{
 		arguments.push_back(std::string(argv[index]));
 	}
+	return arguments;
+}
+
+static void SeedRandomNumberGenerator()
+{
+	srand((unsigned int)time(nullptr));
+}
+
+int main(int argc, char** argv)
+{
+	SeedRandomNumberGenerator();
+	auto arguments = CommandLineToStringVector(argc, argv);
 	return tggd::common::Application::Run(arguments);
 }
