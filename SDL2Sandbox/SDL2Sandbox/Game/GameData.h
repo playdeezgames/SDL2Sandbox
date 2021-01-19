999dc17e7a755cbd05787ef87e8711d0c8c4aae5
ernestpazera <ernestpazera@gmail.com>
2021-01-19 04:01:24 -0600

    minor rearranging and dudefussing over code

--- SDL2Sandbox/SDL2Sandbox/Game/GameData.h
+++ SDL2Sandbox/SDL2Sandbox/Game/GameData.h
@@ -9,29 +9,32 @@ class GameData
 {
 private:
 	const tggd::common::SoundManager& soundManager;
+	GameState& gameState;
+
 	std::vector<int> blockPositions;
 	std::vector<PowerUp> powerUpPositions;
-	int powerUpCounter;
 	std::vector<int> tailPositions;
+
+	int powerUpCounter;
 	int scrollCounter;
-	int direction;
-	GameState& gameState;
+	int scrollCounterMaximum;
+
 	int score;
 	int runLength;
+	int scoreMultiplier;
+	int scoreDivisor;
+	
+	int direction;
 	bool dead;
 	int invincibility;
 	bool keysReversed;
 	int bombs;
 	int lives;
-	int scoreMultiplier;
-	int scoreDivisor;
-	int scrollCounterMaximum;
 	int shield;
 
 	static std::map<PowerUpType, int> powerUpGenerator;
 	static PowerUpType GeneratePowerUp();
 	static int GeneratePowerUpCounter();
-
 
 	void UpdateTail();
 	void UpdateBoard();
@@ -42,26 +45,37 @@ private:
 	void AddScore(int);
 public:
 	GameData(tggd::common::SoundManager&, GameState&);
+
 	void ResetGame();
 	void RestartGame();
 	void NextRun();
+
 	void SetNextDirection(int);
+
 	int GetTailLength() const;
 	int GetTailPosition(int) const;
+
 	int GetBlockCount() const;
 	int GetBlockPosition(int) const;
+
 	int GetPowerUpCount() const;
 	int GetPowerUpPosition(int) const;
 	PowerUpType GetPowerUp(int) const;
-	int GetScore() const;
+
 	PlayerState GetState() const;
-	void Update(int);
-	void UseBomb();
-	int GetBombs() const;
-	void LoseLife();
-	int GetLives() const;
+
+	int GetScore() const;
 	int GetScoreMultiplier() const;
 	int GetScoreDivisor() const;
+
+	void Update(int);
+
+	void UseBomb();
+	int GetBombs() const;
+
+	void LoseLife();
+	int GetLives() const;
+
 	int GetShield() const;
 };
 
